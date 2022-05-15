5245eea47c1eeacf71eb47e82130ec13f1df7e03
Sayam Gautam <sayamgautam1@gmail.com>
2022-05-15 20:55:02 +0930

    add fn in index.html to get manager details

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import rqquired files
+// import required files
 // generate Markdown file
 
 const generateMarkdown = require("./src/generateMarkdown");
@@ -13,3 +13,34 @@ const Intern = require("./lib/Intern");
 // node modules
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+// application starts, with entering manager details
+
+function getManagerDetails() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "Enter the team manager name?",
+    },
+    {
+      type: "input",
+      name: "managerID",
+      message: "Enter the manager employee ID",
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "Enter the manager's email adress",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "Enter the office number",
+    },
+  ]);
+}
+getManagerDetails().then((data) => {
+  //   addEmployes(data);
+  console.log(data);
+});
