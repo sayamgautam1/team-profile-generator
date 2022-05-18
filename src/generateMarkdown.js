22f81a35ea5e1217235b553b235b1a932a6e1b98
Sayam Gautam <sayamgautam1@gmail.com>
2022-05-18 12:57:51 +0930

    add logic to check employee array and retreive data for intern and engineer

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,8 +1,27 @@
 function generateMarkdown(data) {
+  let manager = data.Manager;
+  generateManager(manager);
   let employeeList = data.getEmployees();
   employeeList.forEach((employ) => {
-    console.log(employ.getRole());
+    if (employ.getRole() === "Engineer") {
+      generateEngineer(employ);
+    } else {
+      generateIntern(employ);
+    }
   });
 }
 
+// function to generate maanger card
+function generateManager(data) {
+  console.log(data);
+}
+//function to generate engineer card
+function generateEngineer(data) {
+  console.log(data);
+}
+
+// function to generate intern card
+function generateIntern(data) {
+  console.log(data);
+}
 module.exports = generateMarkdown;
