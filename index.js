365fd2af2a8c02d4c560f3d522803d40a78864ed
Sayam Gautam <sayamgautam1@gmail.com>
2022-05-18 10:44:29 +0930

    fn to recieve manager info

--- index.js
+++ index.js
@@ -17,30 +17,69 @@ const fs = require("fs");
 // application starts, with entering manager details
 
 function getManagerDetails() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "managerName",
-      message: "Enter the team manager name?",
-    },
-    {
-      type: "input",
-      name: "managerID",
-      message: "Enter the manager employee ID",
-    },
-    {
-      type: "input",
-      name: "managerEmail",
-      message: "Enter the manager's email adress",
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "Enter the office number",
-    },
-  ]);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "Enter the team manager name?",
+      },
+      {
+        type: "input",
+        name: "managerID",
+        message: "Enter the manager employee ID",
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "Enter the manager's email adress",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter the office number",
+      },
+    ])
+    .then((details) => {
+      const { managerName, managerId, managerEmail, officeNumber } = details;
+      const managerObj = new Manager(
+        managerName,
+        managerId,
+        managerEmail,
+        officeNumber
+      );
+      team.setManager(managerObj);
+    });
 }
+
+// function to ask if engineer or intern
+
+// working function
 getManagerDetails().then((data) => {
-  //   addEmployes(data);
-  console.log(data);
+  getEmployeeInfo();
 });
+
+class Team {
+  constructor() {
+    this.Manager = null;
+    this.Employee = [];
+  }
+
+  setManager(details) {
+    this.Manager = details;
+    return this.Manager;
+  }
+
+  setEmployee(details) {
+    this.Employee.push();
+  }
+}
+const team = new Team();
+
+// getManagerDetails()
+//   .then(saveManagerDetails)
+//   .then(getEmployeeChoice)
+//   .then((choice) => {
+//     if (choise === "intern/engineer") {
+//     }
+//   });
